f5a85e7760ffe93cd669bc62df6d361daa4f3f0d
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-16 22:26:40 -0300

    Estilizacao dos templates

--- embrapa.py
+++ embrapa.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', titulo='Embrapa - Vitivicultura')
+    return render_template('index.html', 
+                           titulo='Embrapa - Vitivicultura')
 
 
 @app.route('/producao', methods=['POST', 'GET'])
@@ -17,10 +18,17 @@ def producao():
         df = scrape_viti_producao(user_year)
         df_html = df.to_html(index=False)
 
-        return render_template('producao.html', table=df_html, user_year=user_year, titulo=' Embrapa - Produção')
+        return render_template('producao.html', 
+                                table=df_html, 
+                                user_year=user_year, 
+                                titulo=' Embrapa - Produção',
+                                nome = 'Produção')
     
     elif request.method == 'GET':
-        return render_template('producao.html', table=None, titulo=' Embrapa - Produção')
+        return render_template('producao.html', 
+                               table=None, 
+                               titulo=' Embrapa - Produção', 
+                               nome = 'Produção')
     
 @app.route('/comercialização', methods=['POST', 'GET'])
 def comercializacao():
@@ -30,10 +38,17 @@ def comercializacao():
         df = scrape_viti_comercializacao(user_year)
         df_html = df.to_html(index=False)
 
-        return render_template('comercializacao.html', table=df_html, user_year=user_year, titulo=' Embrapa - Comercialização')
+        return render_template('comercializacao.html', 
+                               table=df_html, 
+                               user_year=user_year, 
+                               titulo=' Embrapa - Comercialização', 
+                               nome = 'Comercialização')
     
     elif request.method == 'GET':
-        return render_template('producao.html', table=None, titulo=' Embrapa - Comercialização')
+        return render_template('producao.html', 
+                               table=None, 
+                               titulo=' Embrapa - Comercialização', 
+                               nome = 'Comercialização')
 
 if __name__ == "__main__":
     app.run(debug=True)
