8d2887f732514ea1ef7489d84317c442a2706bbd
Reryson Farinha <rerysonfarinha@outlook.com>
2024-05-17 11:30:32 -0300

    Adicionando logica de autorizacao pelo login

--- embrapa.py
+++ embrapa.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 from web_scraping_py.producao import scrape_viti_producao
 from web_scraping_py.comercializacao import scrape_viti_comercializacao
 
@@ -12,6 +12,10 @@ def index():
 
 @app.route('/producao', methods=['POST', 'GET'])
 def producao():
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+
     if request.method == 'POST':
         user_year = int(request.form['ano'])
 
@@ -30,6 +34,10 @@ def producao():
     
 @app.route('/comercialização', methods=['POST', 'GET'])
 def comercializacao():
+
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+
     if request.method == 'POST':
         user_year = int(request.form['ano'])
 
@@ -51,21 +59,21 @@ def comercializacao():
 def login():
     return render_template('login.html')
 
-@app.route('/autenticar', methods=['POST','GET'])
+@app.route('/autenticar', methods=['POST'])
 def autenticar():
     if 'teste' == request.form['senha']:
         session['usuario_logado'] = request.form['usuario']
-        flash(session['usuario_logado'] + ' logado com sucesso!')
-        return redirect('/')
+        flash(session['usuario_logado'] + ' logado com sucesso!', 'success')
+        return redirect(url_for('index'))
     else:
-        flash('Login não efetuado.')
-        return redirect('/login')
+        flash('Login não efetuado.', 'error')
+        return redirect(url_for('login'))
     
 @app.route('/logout')
 def logout():
-    session['usuario_logado'] = None
-    flash('Usuário deslogado com sucesso.')
-    return redirect('/')
+    session.pop('usuario_logado', None) 
+    flash('Usuário deslogado com sucesso.', 'success')
+    return redirect(url_for('login'))
 
 if __name__ == "__main__":
     app.run(debug=True)
